448f3ed265f66728be7fc9d756d770b35d87f659
Konstantin Tretyakov <konstantin.tretjakov@gmail.com>
2023-12-21 16:17:36 +0100

    Add a fzf-based "insert path from somewhere on screen" widget.

---
+++ .zsh/fzf_path_inserter.sh
@@ -0,0 +1,15 @@
+# Opens a fzf selector to pick from filenames present in the current window
+
+__path_inserter() {
+    tmux capture-pane -pJ -S 0 | grep -o '/[^" '"'"']*' | fzf
+}
+
+fzf-path-inserter-widget() {
+    LBUFFER+="$(__path_inserter)"
+    local ret=$?
+    zle reset-prompt
+    return $ret
+}
+zle -N fzf-path-inserter-widget
+bindkey ^f fzf-path-inserter-widget
+
